d789b2c5af99e5576ce3abb23d29787d7f156c45
Hayes <hayes.miaomiao@gmail.com>
2026-06-30 14:14:44 +0800

    Add app/head.tsx for GSC verification

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="ctNANGYLL54OoiTJnGWujY2DcqiRnsfz0nA-a-2xQm0"
+      />
+    </>
+  );
+}
